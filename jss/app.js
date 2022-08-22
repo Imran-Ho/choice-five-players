05abeaf24be0ffe04a62866c62e352b75a43c438
Imran-Ho <imranhossain36@gmail.com>
2022-08-22 15:18:07 +0600

    players added

--- jss/app.js
+++ jss/app.js
@@ -14,10 +14,28 @@ document.getElementById('babar-btn').addEventListener('click', function(){
     const playersSelection = document.getElementById('babar');
     const createList =document.createElement('li');
     createList.innerText = playersSelection.innerText;
+    createList.classList.add('item');
     playersSelectionList.appendChild(createList);
 
 })
 
+document.getElementById('calc-btn').addEventListener('click',function(){
+    const playerListedId = document.getElementById('players-list');
+    const allChildren = playerListedId.getElementsByTagName('*').length;
+    const playerListedString = parseInt(allChildren);
+
+    const perPlayerId = document.getElementById('per-player-amount');
+    const perPlayerAmountString = perPlayerId.value;
+    const perPlayerAmount = parseInt(perPlayerAmountString);
+
+    const playersTotalId = document.getElementById('player-expense');
+    const playersTotal = playerListedString * perPlayerAmount;
+    playersTotalId.innerText = playersTotal;
+    
+})
+
+
+// total amount
 document.getElementById('totalCalc-btn').addEventListener('click',function(){
     const playerExpense = document.getElementById('player-expense');
     const playerExpenseString = playerExpense.innerText;
@@ -30,6 +48,10 @@ document.getElementById('totalCalc-btn').addEventListener('click',function(){
     const coachExpense = document.getElementById('coach-expense');
     const coachExpenseString = coachExpense.value;
     const coachSalary = parseInt(coachExpenseString);
+
+    const totalElement = document.getElementById('total');
+    const totalFigure = previousPlayerExpense + managerSalary + coachSalary;
+    totalElement.innerText = totalFigure;
 })
 
 
